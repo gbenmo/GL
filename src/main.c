41e43834c46569217381b176f5d8b0de804a6868
gbenmo <ghais.ben-mohamed@u-psud.fr>
2018-11-14 22:29:20 +0100

    Exo 45

--- src/main.c
+++ src/main.c
@@ -2,12 +2,43 @@
 
 void main (void)
 {
-	short resultat, i=0;
 	
-	while (i<10)  // On effectue la boucle pour 10 valeurs .
+	unsigned short int points = 0;  // Déclaration des variables points et resultat.   
+	
+	unsigned short int resultat= 0;
+	
+	 
+	do						// Effectuer la boucle tant que la valeur obtenue est différente de 1. 
 		
 	{
-		resultat = LancerDe (); // Appel de la fonction LancerDe de la librairie.
-		i++;  // On incrémente i.
-	}
+		resultat = LancerDe ();
+		
+		
+		 if ( resultat == 2 || resultat == 4 || resultat == 6)   // Si la valeur obtenue est 2 ,4 ou 6. 
+		 {
+			 
+			 points = points + resultat; // On ajoute au point la valeur du dé obtenue.
+			 
+		 }
+		 
+		 else if ( resultat == 3)  // Si la valeur obtenue est 3.
+			 
+		 {
+			 
+			 points = points * 2 ;   // On double la valeur des points.
+			 
+		 }
+
+		 else   // Sachant que tout les nombre sdu dés sont utiliser on tombera forcement sur 5 si cette condition est vraie .
+			 
+		 {
+			 
+		 points = points - 2 ; // On soustrait 2 points ( Sachant que points est Unsigned il ne sera pas inferieur à 0 ).
+		 
+		 }
+	} 
+	
+	while (resultat != 1);
+	
+	
 }
