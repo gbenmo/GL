aad3239cdcdddb18f08bcd9d9ea34a2aeeff2da1
gbenmo <ghais.ben-mohamed@u-psud.fr>
2018-11-14 22:30:37 +0100

    Début de l'exo 43

--- src/main.c
+++ src/main.c
@@ -1,9 +1,61 @@
-//  Copyright (c) 2018 Antoine Tran Tan
-//
+#include <stdlib.h>
 
-#include "my_header.h"
 
-int main(void)
+void main (void)		   // Déclaration des suites et variables
 {
-    return 0;
+	unsigned short int S1 = 0; 
+	
+	unsigned long int S2 = 0;
+	
+	unsigned short int S3 = 0;
+	
+	unsigned short int S4 = 0;
+	
+	unsigned char n = 1  ;
+	
+	int i = 0 ;
+	 
+	int nb_rand;
+	
+	
+	
+	
+					
+	  while ( S1 < 360 )	// Calcul des premiers termes des multiples 3.
+		  
+	{
+		
+	 S1 = S1 + 3*n ;
+		 						
+	 n = n + 1 ;
+		 
+	}
+	
+	n = 0; 
+	
+	while ( S2 < 9876 )	   // Calcul des multiples de 3 inférieur a 9876.
+		  
+	{
+		
+	 S2 = S2 + 3*n ;
+		 						
+	 n = n + 1 ;
+		 
+	}
+	
+	while ( i < 12 )   // on effectue la boucle pour 12 valeurs.
+		
+	{
+		nb_rand = rand()%11; // On utlise modulo pour obtenir un nombre entre 0 et 10 ,car 11 exlcue !  
+		
+		S3 = S3 + nb_rand ;
+		
+		i++; 
+	
+	}
+	
+	
 }
+		
+		
+		
