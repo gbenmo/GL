6ef4d84d8942d6014701016f8bbf3c2959169b2c
ghais ben-mohamed <ghais.ben-mohamed@ge1.iut-cachan.u-psud.fr>
2018-11-20 11:28:32 +0100

    Debut de l'exo56

--- src/main.c
+++ src/main.c
@@ -5,5 +5,27 @@
 
 int main(void)
 {
-    return 0;
+	  unsigned char bin [10] = {230, 120, 200, 50, 86, 127, 254, 21 , 89, 2};
+	  int SEUIL = 150;
+	  int i=0;
+	  
+	  for ( i=0; i < 10; i++)
+		  
+	  {
+		    if ( bin[i] > SEUIL)
+			{
+		  bin[i] = 1;
+			}
+			
+			else
+			{
+				bin[i]=0;
+			}
+		  
+ 
+	  }
+			   
+	  
+	 return 0;
+    
 }
