68b0fec7915b36178c3cc8a8a9ac64eea04e718c
ghais ben-mohamed <ghais.ben-mohamed@ge1.iut-cachan.u-psud.fr>
2018-11-27 11:45:26 +0100

    Debut exo 58

--- src/TP_lib_suite.h
+++ src/TP_lib_suite.h
@@ -14,21 +14,3 @@ Fichier : TP_lib_suite.H
 void  initialiserTirage(void);
 short tirerNumero(void);
 
-/*----------- genetique ------------*/
-char  tirerNucleotide(void);    
-
-/*----------- lancer dé ------------*/
-long int  JouerUnTour ( void ) ;  
-
-
-/*----------- divers ------------*/
-
-int estPremier(long nombre);
-
-void faireUnTour(void);  
-
-void afficherChiffre(short chiffre);
-
-void afficherCompteARebours(short compteur);
-
-double f(double x);
--- src/main.c
+++ src/main.c
@@ -2,8 +2,30 @@
 //
 
 #include "my_header.h"
+#include "TP_lib_suite.h"
 
 int main(void)
 {
+	
+	 unsigned char joueur [6] = {1,21,36,12,23,8};
+	 unsigned char gagnant [6] = { 1,47,24,26,3,5 };
+	 unsigned char i;
+	 unsigned char j;
+	 unsigned char valide;
+	 
+	 for ( i=0 ;i<6; i++ )
+	 {
+		 
+		 if ( joueur [i] == gagnant [j] )
+			 
+		 {
+			 valide++;
+		 }
+			 
+				 
+	 
+
+	
     return 0;
+}
 }
