6d0d2c721b5185b415ce472502c53b47db780dd1
ghais ben-mohamed <ghais.ben-mohamed@ge1.iut-cachan.u-psud.fr>
2018-12-04 11:21:04 +0100

    debut exo 79

--- src/main.c
+++ src/main.c
@@ -1,9 +1,31 @@
+
 //  Copyright (c) 2018 Antoine Tran Tan
 //
 
-#include "my_header.h"
+#include <ansi_c.h>  
 
 int main(void)
 {
-    return 0;
+      char prenom [100];
+	  char nom [100];
+	  char age [100];
+	  
+	  
+	  scanf ( "%s", prenom);
+	  
+	  scanf ( "%s", nom);
+	  
+	  scanf ( "%s", age);
+	  
+	  printf ( "Je m'apelle %s %s et j'ai %s ans", prenom,nom,age);
+	  
+	  
+	  getchar();
+	  
+	  
+	  
+	  
+	  
+	  
+	  
 }
