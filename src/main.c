adb7c94566bbacc0eed212625d1a1a36d3c19591
ghais ben-mohamed <ghais.ben-mohamed@ge1.iut-cachan.u-psud.fr>
2018-12-04 11:40:26 +0100

    suite

--- src/main.c
+++ src/main.c
@@ -2,13 +2,16 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
 
-#include <ansi_c.h>  
+#include <ansi_c.h> 
+#include <userint.h>
 
 int main(void)
 {
       char prenom [100];
 	  char nom [100];
 	  char age [100];
+	  char Bienvenue [9] = " Bienvenue";
+	  int a;
 	  
 	  
 	  scanf ( "%s", prenom);
@@ -19,6 +22,8 @@ int main(void)
 	  
 	  printf ( "Je m'apelle %s %s et j'ai %s ans", prenom,nom,age);
 	  
+	  
+	  a= MessagePopup (Bienvenue ,phrase);
 	  
 	  getchar();
 	  
